752e80cf2f017f7c937ad7a05865a71f83c5e4b2
Piyushnegi04 <piyushsinghnegi1@gmail.com>
2024-03-26 15:48:50 +0530

    range function in python....

---
+++ rangefunction.py
@@ -0,0 +1,30 @@
+#range- it will give the no. of iteration based on indexing
+#x = range(11) #end index-1
+# # print(x[6])
+
+#for i in x:
+#    print(i)
+
+#x= range(2,11) #start index, end index-1
+#print(x[1])
+
+# for i in x:
+#     print(i)
+
+#x= range(2,11,2) #start index, end index-1, step size
+#print(x[1])
+
+# for i in x:
+#     print(i)
+
+# x= range(1,-10,-1) #-ve values
+#print(x[1])
+
+# for i in x:
+#     print(i)
+
+x = range(1001) #reversed value
+
+
+for i in x:
+    print(i)
